fabd8e8c14deef23b2329ade4a559070a69c16f6
Padmakshi Ashok Kumar <padmakshi.ashok@gmail.com>
2023-03-05 23:17:07 -0800

    Adding the getSubscriberByDeal and DeleteSubscriber

--- src/main/java/ds/project/deals/subscribers/repository/SubscriberRepository.java
+++ src/main/java/ds/project/deals/subscribers/repository/SubscriberRepository.java
@@ -1,6 +1,15 @@
 package ds.project.deals.subscribers.repository;
 
 import ds.project.deals.subscribers.entity.Subscriber;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
-public interface SubscriberRepository extends CrudRepository<Subscriber, Integer> {}
+public interface SubscriberRepository extends CrudRepository<Subscriber, Integer> {
+	
+	List<Subscriber> findBySubFixedDealsId(int dealId);
+	
+	List<Subscriber> findBySubShopId(int shopId);
+	
+}
--- src/main/java/ds/project/deals/subscribers/service/SubscriberService.java
+++ src/main/java/ds/project/deals/subscribers/service/SubscriberService.java
@@ -1,5 +1,6 @@
 package ds.project.deals.subscribers.service;
 
+import ds.project.deals.deals.entity.Deals;
 import ds.project.deals.fixed_deals.entity.FixedDeals;
 import ds.project.deals.fixed_deals.repository.FixedDealsRepository;
 import ds.project.deals.shops.entity.Shop;
@@ -9,6 +10,9 @@ import ds.project.deals.subscribers.repository.SubscriberRepository;
 import ds.project.deals.users.entity.User;
 import ds.project.deals.users.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -51,6 +55,30 @@ public class SubscriberService {
         return subscriberRepository.save(subscriber);
     }
 
+    // to be moved to Sruthi's Repo:
+    public List<Subscriber> getSubscriberByDeal(Deals deal) {
+    	
+    	if(deal == null || deal.getId() == 0) {
+    		throw new EntityNotFoundException("Deal or Deal id not specified.");
+    	}
+    	
+    	if(deal.getId() == 1) {
+    		if(deal.getShop() == null || deal.getShop().getId() == 0) {
+    			throw new EntityNotFoundException("Shop or Shop id not specified.");
+    		}
+    		return subscriberRepository.findBySubShopId(deal.getShop().getId());
+    	} else {
+    		return subscriberRepository.findBySubFixedDealsId(deal.getId());
+    	}
+    }
+    
+    public void deleteSubscriber(Subscriber subscriber) {
+    	User user = userRepository.findById(subscriber.getUser().getId())
+    			.orElseThrow(() -> new EntityNotFoundException("User not found with id : " + subscriber.getUser().getId()));
+    	subscriberRepository.deleteById(user.getId());
+    }
+    
+    //Till here
     public Subscriber getSubscriberById(int id) {
         Subscriber subscriber = subscriberRepository.findById(id)
                                     .orElseThrow(() -> new EntityNotFoundException("Subscriber not found with Id" + id));
